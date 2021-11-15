622f3685a2964800748c07270ab9ae8436bcfa44
Jaehoon Jang <jangjh971008@naver.com>
2021-11-16 00:20:58 +0900

    add: 상품 상세 페이지 구상

--- lib/screens/Feed/Details.screen.dart
+++ lib/screens/Feed/Details.screen.dart
@@ -9,6 +9,9 @@ class DetailsScreen extends StatefulWidget {
 }
 
 class _DetailsScreenState extends State<DetailsScreen> {
+  String bidPrice = '39,000';
+  String buyPrice = '50,000';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,95 +20,112 @@ class _DetailsScreenState extends State<DetailsScreen> {
         shadowColor: const Color.fromRGBO(0, 0, 0, 0),
         iconTheme: const IconThemeData(color: Colors.black),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.bookmark_add),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                width: 130,
-                height: 50,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "입찰",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      const VerticalDivider(
-                        color: Colors.white,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "39,000",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Text(
-                            "현재 입찰가",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      )
-                    ]),
-                decoration: BoxDecoration(
-                  color: Color(0xff0064FF),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            const Spacer(),
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                width: 130,
-                height: 50,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "구매",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      const VerticalDivider(
-                        color: Colors.white,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "50,000",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Text(
-                            "즉시 구매가",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      )
-                    ]),
-                decoration: BoxDecoration(
-                  color: Color(0xffb90000),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            const Spacer(),
+            Image.asset('lib/assets/images/emptyImage.png'),
+            Text("상품명"),
+            Text("카테고리"),
+            Text("남은 시간 02 : 08 : 24"),
+            Text("상품 설명"),
+            Text("처음 가격 / 입찰 횟수"),
+            Text("찜한 사람")
           ],
         ),
       ),
-      body: Column(),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.bookmark_add),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 130,
+                  height: 50,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "입찰",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        const VerticalDivider(
+                          color: Colors.white,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              bidPrice,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            const Text(
+                              "현재 입찰가",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        )
+                      ]),
+                  decoration: BoxDecoration(
+                    color: Color(0xff0064FF),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 130,
+                  height: 50,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "구매",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        const VerticalDivider(
+                          color: Colors.white,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              buyPrice,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            const Text(
+                              "즉시 구매가",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        )
+                      ]),
+                  decoration: BoxDecoration(
+                    color: Color(0xffb90000),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
