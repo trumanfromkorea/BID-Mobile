e8853681bb2d524c2437f3b8d80a712402730039
Jaehoon Jang <jangjh971008@naver.com>
2021-11-17 23:54:11 +0900

    test: api 에서 받아온 리스트 렌더링하기 (미완)
    json list 형태로 받아온 api 를 고려하지 않고
    class 를 구성하여 에러가 발생함
    내일 출근해서 혹은 퇴근하고 완성해보자

--- lib/screens/Feed/Feed.screen.dart
+++ lib/screens/Feed/Feed.screen.dart
@@ -4,6 +4,40 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+Future<Post> fetchPost() async {
+  String url = 'http://127.0.0.1:8000/api/product/';
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    print("api 호출 성공 : ${json.decode(response.body)}");
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('api 호출 실패');
+  }
+}
+
+class Post {
+  final int id;
+  final String name;
+  final int price;
+  final String createdAt;
+
+  Post(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.createdAt});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      name: json['name'],
+      price: json['price'],
+      createdAt: json['created_at'],
+    );
+  }
+}
+
 class FeedScreen extends StatefulWidget {
   const FeedScreen({Key? key}) : super(key: key);
 
@@ -12,56 +46,40 @@ class FeedScreen extends StatefulWidget {
 }
 
 class _FeedScreenState extends State<FeedScreen> {
-  List? feedList;
+  late Future<Post> post;
 
   onPressDetails() {
     Navigator.push(context,
         MaterialPageRoute(builder: (context) => const DetailsScreen()));
   }
 
-  void initFeedList() async {
-    String url = 'http://127.0.0.1:8000/api/product/';
-    var response = await http.get(Uri.parse(url));
-
-    setState(() {
-      feedList = jsonDecode(response.body);
-    });
-    // print(jsonDecode(response.body));
-  }
-
   @override
   initState() {
     super.initState();
+    post = fetchPost();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (feedList != null) {
-      return Scaffold(
-        body: Column(
-          children: [
-            const SafeArea(child: SizedBox()),
-            CupertinoButton(
-                child: const Text("상세 정보"), onPressed: onPressDetails),
-            Expanded(
-              child: ListView.builder(
-                itemCount: feedList?.length,
-                itemBuilder: (context, int index) {
-                  return Container(
-                    child: Column(
-                      children: [
-                        Text(feedList?[index]['name']),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
-        ),
-      );
-    } else {
-      return Container();
-    }
+    return Scaffold(
+      body: Column(
+        children: [
+          const SafeArea(child: SizedBox()),
+          CupertinoButton(
+              child: const Text("상세 정보"), onPressed: onPressDetails),
+          FutureBuilder(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.toString());
+              } else {
+                return Text("error : ${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            },
+          )
+        ],
+      ),
+    );
   }
 }
