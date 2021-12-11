a999fa7845880330e63951fbf0123ea725679a3f
Jaehoon Jang <jangjh971008@naver.com>
2021-12-11 20:15:45 +0900

    feat: feed list 분리중

--- lib/screens/Feed/Feed.screen.dart
+++ lib/screens/Feed/Feed.screen.dart
@@ -1,5 +1,8 @@
+import 'package:bid_mobile/data/mainTheme.dart';
 import 'package:bid_mobile/screens/Feed/Details.screen.dart';
 import 'package:bid_mobile/screens/Feed/Write.screen.dart';
+import 'package:bid_mobile/widgets/Feed/list_candidate.widget.dart';
+import 'package:bid_mobile/widgets/Feed/list_running.widget.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -54,8 +57,8 @@ class FeedScreen extends StatefulWidget {
   _FeedScreenState createState() => _FeedScreenState();
 }
 
-class _FeedScreenState extends State<FeedScreen> {
-  // var sampleData = [1, 2, 3, 4, 5, 6, 7];
+class _FeedScreenState extends State<FeedScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
 
   TextStyle keyText = const TextStyle(
     fontSize: 13,
@@ -70,8 +73,7 @@ class _FeedScreenState extends State<FeedScreen> {
   );
 
   onPressDetails() {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => const DetailsScreen()));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const DetailsScreen()));
   }
 
   List dataList = [];
@@ -88,6 +90,7 @@ class _FeedScreenState extends State<FeedScreen> {
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(vsync: this, length: 2);
     initList();
   }
 
@@ -101,141 +104,48 @@ class _FeedScreenState extends State<FeedScreen> {
           ),
           shadowColor: const Color.fromRGBO(0, 0, 0, 0),
           backgroundColor: Colors.white,
+          bottom: TabBar(
+            unselectedLabelColor: const Color(0xffcdcdcd),
+            labelColor: mainThemeColor,
+            controller: _tabController,
+            tabs: const [
+              Tab(
+                child: Text(
+                  "진행 중",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "진행 예정",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        body: Container(
-          child: ListView.builder(
-              itemCount: dataList.length,
-              itemBuilder: (context, int index) {
-                String dateString = dataList[index]['expireDate'].split('T')[0];
-
-                return GestureDetector(
-                  onTap: onPressDetails,
-                  child: Container(
-                      alignment: Alignment.center,
-                      // margin: const EdgeInsets.symmetric(vertical: 5),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 25, vertical: 25),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Colors.black38,
-                            width: 0.5,
-                          ),
-                        ),
-                        // borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(right: 15),
-                            width: MediaQuery.of(context).size.width * 0.25,
-                            height: MediaQuery.of(context).size.width * 0.25,
-                            decoration: BoxDecoration(
-                              color: Colors.black12,
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.only(bottom: 5),
-                                child: Text(
-                                  dataList[index]['name'],
-                                  style: const TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                              RichText(
-                                text: TextSpan(
-                                    style: const TextStyle(
-                                      fontFamily: 'NanumSquare',
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                        text: "입찰가 ",
-                                        style: keyText,
-                                      ),
-                                      TextSpan(
-                                        text: dataList[index]['startPrice']
-                                                .toString() +
-                                            "원",
-                                        style: valueText,
-                                      )
-                                    ]),
-                              ),
-                              RichText(
-                                text: TextSpan(
-                                    style: const TextStyle(
-                                      fontFamily: 'NanumSquare',
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                        text: "입찰수 ",
-                                        style: keyText,
-                                      ),
-                                      TextSpan(
-                                        text: dataList[index]['bidCount']
-                                                .toString() +
-                                            "회",
-                                        style: valueText,
-                                      )
-                                    ]),
-                              ),
-                              RichText(
-                                text: TextSpan(
-                                    style: const TextStyle(
-                                      fontFamily: 'NanumSquare',
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                        text: "마감일 ",
-                                        style: keyText,
-                                      ),
-                                      TextSpan(
-                                        text: dateString,
-                                        style: valueText,
-                                      )
-                                    ]),
-                              ),
-                            ],
-                          )
-                        ],
-                      )),
-                );
-              }),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListRunning(
+              dataList: dataList,
+            ),
+            ListCandidate(
+              dataList: dataList,
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const WriteScreen()));
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const WriteScreen()));
           },
           child: const Icon(Icons.add),
         ));
-  }
-}
-
-class PostsList extends StatelessWidget {
-  const PostsList({Key? key, required this.posts}) : super(key: key);
-
-  final List<Post> posts;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: posts.length,
-      itemBuilder: (context, index) {
-        return Column(
-          children: [
-            Text(posts[index].name),
-          ],
-        );
-      },
-    );
   }
 }
---
+++ lib/widgets/Feed/list_running.widget.dart
@@ -0,0 +1,132 @@
+import 'package:bid_mobile/screens/Feed/Details.screen.dart';
+import 'package:flutter/material.dart';
+
+class ListRunning extends StatefulWidget {
+  const ListRunning({Key? key, required this.dataList}) : super(key: key);
+
+  final List dataList;
+
+  @override
+  _ListRunningState createState() => _ListRunningState();
+}
+
+class _ListRunningState extends State<ListRunning> {
+  TextStyle keyText = const TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: Colors.black45,
+  );
+
+  TextStyle valueText = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.dataList.length,
+      itemBuilder: (context, int index) {
+        String dateString = widget.dataList[index]['expireDate'].split('T')[0];
+
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const DetailsScreen()));
+          },
+          child: Container(
+            alignment: Alignment.center,
+            // margin: const EdgeInsets.symmetric(vertical: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.black38,
+                  width: 0.5,
+                ),
+              ),
+              // borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 15),
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  height: MediaQuery.of(context).size.width * 0.25,
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: Text(
+                        widget.dataList[index]['name'],
+                        style: const TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                          style: const TextStyle(
+                            fontFamily: 'NanumSquare',
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "입찰가 ",
+                              style: keyText,
+                            ),
+                            TextSpan(
+                              text: widget.dataList[index]['startPrice'].toString() + "원",
+                              style: valueText,
+                            )
+                          ]),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                          style: const TextStyle(
+                            fontFamily: 'NanumSquare',
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "입찰수 ",
+                              style: keyText,
+                            ),
+                            TextSpan(
+                              text: widget.dataList[index]['bidCount'].toString() + "회",
+                              style: valueText,
+                            )
+                          ]),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                          style: const TextStyle(
+                            fontFamily: 'NanumSquare',
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "마감일 ",
+                              style: keyText,
+                            ),
+                            TextSpan(
+                              text: dateString,
+                              style: valueText,
+                            )
+                          ]),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
